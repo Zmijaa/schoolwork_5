25d26f0b5d8811c8e98eed1886ca21eb8481cb1a
Filip Mircev <147530688+Zmijaa@users.noreply.github.com>
2023-11-30 09:11:17 +0100

    Add files via upload

---
+++ src/Kruznica.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Kruznica {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+double perimetar,r,p;
+Scanner tastatura=new Scanner(System.in);
+System.out.println("presmetka na perimetar i plostina na kruznica");
+double p1=3.14;
+System.out.println("vnesete dolzina na r-radius");
+r=tastatura.nextDouble();
+double perimetar1=2*r*p1;
+System.out.println();
+System.out.println("rezultat");
+System.out.println("perimetarot e"+perimetar1);
+
+
+
+	}
+
+}
